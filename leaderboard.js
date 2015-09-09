e11b0a810188e9849a05d2a5fc1edfbdd10c3ecd
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-09 16:53:33 -0500

    ES6ification

--- leaderboard.js
+++ leaderboard.js
@@ -1,50 +1,61 @@
+/* ES6 notes
+  semi-colons are not required (never have been FYI)
+  object literal short-hand lets you omit colon ':' and function keyword
+  'return' is optional for single-expression arrow functions: (e) => e.target
+  braces '{}' are required for multiple-statement arrow functions:
+     (e) => {
+       let $target = $(e.target)
+       return $target.val()
+     }
+  argument parens are optional for single-argument arrow functions: e => e.target
+  object literal {name: name} can be simply {name}
+*/
+
 // Set up a collection to contain player information. On the server,
 // it is backed by a MongoDB collection named "players".
 
-Players = new Mongo.Collection("players");
+Players = new Mongo.Collection("players")
 
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
-    players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1 } });
-    },
-    selectedName: function () {
-      var player = Players.findOne(Session.get("selectedPlayer"));
-      return player && player.name;
+    players() {return Players.find({}, { sort: { score: -1, name: 1 } })},
+    selectedName() {
+      let player = Players.findOne(Session.get("selectedPlayer"))
+      return player && player.name
     }
-  });
+  })
 
   Template.leaderboard.events({
-    'click .inc': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
+    'click .inc'() {
+      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}})
     }
-  });
+  })
 
   Template.player.helpers({
-    selected: function () {
-      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
+    selected() {
+      return Session.equals("selectedPlayer", this._id) ? "selected" : ''
     }
-  });
+  })
 
   Template.player.events({
-    'click': function () {
-      Session.set("selectedPlayer", this._id);
+    click() {
+      Session.set("selectedPlayer", this._id)
     }
-  });
+  })
 }
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(() => {
     if (Players.find().count() === 0) {
       var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-      _.each(names, function (name) {
+                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"]
+      names.forEach(name =>
         Players.insert({
-          name: name,
+          name,
           score: Math.floor(Random.fraction() * 10) * 5
-        });
-      });
+        })
+      )
     }
-  });
+  })
 }
